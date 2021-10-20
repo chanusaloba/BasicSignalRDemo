1d56f70bafb761b6e20071636e03b12e01b73954
ChanusAloba <chanus.aloba@nzfunds.co.nz>
2021-10-20 14:38:24 +0800

    Add project files.

---
+++ SignalRDemo/Hubs/ChatHub.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMessage(string user, string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public Task SendMessageToCaller(string user, string message)
+        {
+            return Clients.Caller.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public Task SendMessageToGroup(string user, string message)
+        {
+            return Clients.Group("SignalR Users").SendAsync("ReceiveMessage", user, message);
+        }
+
+        [HubMethodName("SendMessageToUser")]
+        public Task DirectMessage(string user, string message)
+        {
+            return Clients.User(user).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public Task ThrowException()
+        {
+            throw new HubException("This error will be sent to the client!");
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ SignalRDemo/Hubs/IChatClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SignalRDemo.Hubs
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(string user, string message);
+    }
+}
